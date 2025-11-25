6186f5348843dae6199d61ce18befcb46a82650d
Alan H. <69955478+hehzz568@users.noreply.github.com>
2025-11-24 22:51:32 -0500

    VGA updated

--- src/background_rom_bb.v
+++ src/background_rom_bb.v
@@ -1,10 +1,10 @@
-// megafunction wizard: %ROM: 1-PORT%VBB%
+// megafunction wizard: %RAM: 1-PORT%VBB%
 // GENERATION: STANDARD
 // VERSION: WM1.0
 // MODULE: altsyncram 
 
 // ============================================================
-// File Name: title_rom.v
+// File Name: background_rom.v
 // Megafunction Name(s):
 // 			altsyncram
 //
@@ -31,13 +31,17 @@
 //Intel and sold by Intel or its authorized distributors.  Please
 //refer to the applicable agreement for further details.
 
-module title_rom (
+module background_rom (
 	address,
 	clock,
+	data,
+	wren,
 	q);
 
 	input	[18:0]  address;
 	input	  clock;
+	input	[1:0]  data;
+	input	  wren;
 	output	[1:0]  q;
 `ifndef ALTERA_RESERVED_QIS
 // synopsys translate_off
@@ -55,6 +59,7 @@ endmodule
 // Retrieval info: PRIVATE: ADDRESSSTALL_A NUMERIC "0"
 // Retrieval info: PRIVATE: AclrAddr NUMERIC "0"
 // Retrieval info: PRIVATE: AclrByte NUMERIC "0"
+// Retrieval info: PRIVATE: AclrData NUMERIC "0"
 // Retrieval info: PRIVATE: AclrOutput NUMERIC "0"
 // Retrieval info: PRIVATE: BYTE_ENABLE NUMERIC "0"
 // Retrieval info: PRIVATE: BYTE_SIZE NUMERIC "8"
@@ -62,6 +67,7 @@ endmodule
 // Retrieval info: PRIVATE: CLOCK_ENABLE_INPUT_A NUMERIC "0"
 // Retrieval info: PRIVATE: CLOCK_ENABLE_OUTPUT_A NUMERIC "0"
 // Retrieval info: PRIVATE: Clken NUMERIC "0"
+// Retrieval info: PRIVATE: DataBusSeparated NUMERIC "1"
 // Retrieval info: PRIVATE: IMPLEMENT_IN_LES NUMERIC "0"
 // Retrieval info: PRIVATE: INIT_FILE_LAYOUT STRING "PORT_A"
 // Retrieval info: PRIVATE: INIT_TO_SIM_X NUMERIC "0"
@@ -69,42 +75,50 @@ endmodule
 // Retrieval info: PRIVATE: JTAG_ENABLED NUMERIC "0"
 // Retrieval info: PRIVATE: JTAG_ID STRING "NONE"
 // Retrieval info: PRIVATE: MAXIMUM_DEPTH NUMERIC "0"
-// Retrieval info: PRIVATE: MIFfilename STRING "./src/connect8_title.mif"
+// Retrieval info: PRIVATE: MIFfilename STRING "./src/connect8_bg.mif"
 // Retrieval info: PRIVATE: NUMWORDS_A NUMERIC "307200"
 // Retrieval info: PRIVATE: RAM_BLOCK_TYPE NUMERIC "0"
+// Retrieval info: PRIVATE: READ_DURING_WRITE_MODE_PORT_A NUMERIC "3"
 // Retrieval info: PRIVATE: RegAddr NUMERIC "1"
+// Retrieval info: PRIVATE: RegData NUMERIC "1"
 // Retrieval info: PRIVATE: RegOutput NUMERIC "0"
 // Retrieval info: PRIVATE: SYNTH_WRAPPER_GEN_POSTFIX STRING "0"
 // Retrieval info: PRIVATE: SingleClock NUMERIC "1"
-// Retrieval info: PRIVATE: UseDQRAM NUMERIC "0"
+// Retrieval info: PRIVATE: UseDQRAM NUMERIC "1"
+// Retrieval info: PRIVATE: WRCONTROL_ACLR_A NUMERIC "0"
 // Retrieval info: PRIVATE: WidthAddr NUMERIC "19"
 // Retrieval info: PRIVATE: WidthData NUMERIC "2"
 // Retrieval info: PRIVATE: rden NUMERIC "0"
 // Retrieval info: LIBRARY: altera_mf altera_mf.altera_mf_components.all
-// Retrieval info: CONSTANT: ADDRESS_ACLR_A STRING "NONE"
 // Retrieval info: CONSTANT: CLOCK_ENABLE_INPUT_A STRING "BYPASS"
 // Retrieval info: CONSTANT: CLOCK_ENABLE_OUTPUT_A STRING "BYPASS"
-// Retrieval info: CONSTANT: INIT_FILE STRING "./src/connect8_title.mif"
+// Retrieval info: CONSTANT: INIT_FILE STRING "./src/connect8_bg.mif"
 // Retrieval info: CONSTANT: INTENDED_DEVICE_FAMILY STRING "Cyclone V"
 // Retrieval info: CONSTANT: LPM_HINT STRING "ENABLE_RUNTIME_MOD=NO"
 // Retrieval info: CONSTANT: LPM_TYPE STRING "altsyncram"
 // Retrieval info: CONSTANT: NUMWORDS_A NUMERIC "307200"
-// Retrieval info: CONSTANT: OPERATION_MODE STRING "ROM"
+// Retrieval info: CONSTANT: OPERATION_MODE STRING "SINGLE_PORT"
 // Retrieval info: CONSTANT: OUTDATA_ACLR_A STRING "NONE"
 // Retrieval info: CONSTANT: OUTDATA_REG_A STRING "UNREGISTERED"
+// Retrieval info: CONSTANT: POWER_UP_UNINITIALIZED STRING "FALSE"
+// Retrieval info: CONSTANT: READ_DURING_WRITE_MODE_PORT_A STRING "NEW_DATA_NO_NBE_READ"
 // Retrieval info: CONSTANT: WIDTHAD_A NUMERIC "19"
 // Retrieval info: CONSTANT: WIDTH_A NUMERIC "2"
 // Retrieval info: CONSTANT: WIDTH_BYTEENA_A NUMERIC "1"
 // Retrieval info: USED_PORT: address 0 0 19 0 INPUT NODEFVAL "address[18..0]"
 // Retrieval info: USED_PORT: clock 0 0 0 0 INPUT VCC "clock"
+// Retrieval info: USED_PORT: data 0 0 2 0 INPUT NODEFVAL "data[1..0]"
 // Retrieval info: USED_PORT: q 0 0 2 0 OUTPUT NODEFVAL "q[1..0]"
+// Retrieval info: USED_PORT: wren 0 0 0 0 INPUT NODEFVAL "wren"
 // Retrieval info: CONNECT: @address_a 0 0 19 0 address 0 0 19 0
 // Retrieval info: CONNECT: @clock0 0 0 0 0 clock 0 0 0 0
+// Retrieval info: CONNECT: @data_a 0 0 2 0 data 0 0 2 0
+// Retrieval info: CONNECT: @wren_a 0 0 0 0 wren 0 0 0 0
 // Retrieval info: CONNECT: q 0 0 2 0 @q_a 0 0 2 0
-// Retrieval info: GEN_FILE: TYPE_NORMAL title_rom.v TRUE
-// Retrieval info: GEN_FILE: TYPE_NORMAL title_rom.inc FALSE
-// Retrieval info: GEN_FILE: TYPE_NORMAL title_rom.cmp FALSE
-// Retrieval info: GEN_FILE: TYPE_NORMAL title_rom.bsf FALSE
-// Retrieval info: GEN_FILE: TYPE_NORMAL title_rom_inst.v FALSE
-// Retrieval info: GEN_FILE: TYPE_NORMAL title_rom_bb.v TRUE
+// Retrieval info: GEN_FILE: TYPE_NORMAL background_rom.v TRUE
+// Retrieval info: GEN_FILE: TYPE_NORMAL background_rom.inc FALSE
+// Retrieval info: GEN_FILE: TYPE_NORMAL background_rom.cmp FALSE
+// Retrieval info: GEN_FILE: TYPE_NORMAL background_rom.bsf FALSE
+// Retrieval info: GEN_FILE: TYPE_NORMAL background_rom_inst.v FALSE
+// Retrieval info: GEN_FILE: TYPE_NORMAL background_rom_bb.v TRUE
 // Retrieval info: LIB_FILE: altera_mf
--- src/title_rom.v
+++ src/title_rom.v
@@ -43,7 +43,7 @@ module title_rom (
 
 	input	[18:0]  address;
 	input	  clock;
-	output	[7:0]  q;
+	output	[1:0]  q;
 `ifndef ALTERA_RESERVED_QIS
 // synopsys translate_off
 `endif
@@ -52,8 +52,8 @@ module title_rom (
 // synopsys translate_on
 `endif
 
-	wire [7:0] sub_wire0;
-	wire [7:0] q = sub_wire0[7:0];
+	wire [1:0] sub_wire0;
+	wire [1:0] q = sub_wire0[1:0];
 
 	altsyncram	altsyncram_component (
 				.address_a (address),
@@ -71,7 +71,7 @@ module title_rom (
 				.clocken1 (1'b1),
 				.clocken2 (1'b1),
 				.clocken3 (1'b1),
-				.data_a ({8{1'b1}}),
+				.data_a ({2{1'b1}}),
 				.data_b (1'b1),
 				.eccstatus (),
 				.q_b (),
@@ -92,7 +92,7 @@ module title_rom (
 		altsyncram_component.outdata_aclr_a = "NONE",
 		altsyncram_component.outdata_reg_a = "UNREGISTERED",
 		altsyncram_component.widthad_a = 19,
-		altsyncram_component.width_a = 8,
+		altsyncram_component.width_a = 2,
 		altsyncram_component.width_byteena_a = 1;
 
 
@@ -127,7 +127,7 @@ endmodule
 // Retrieval info: PRIVATE: SingleClock NUMERIC "1"
 // Retrieval info: PRIVATE: UseDQRAM NUMERIC "0"
 // Retrieval info: PRIVATE: WidthAddr NUMERIC "19"
-// Retrieval info: PRIVATE: WidthData NUMERIC "8"
+// Retrieval info: PRIVATE: WidthData NUMERIC "2"
 // Retrieval info: PRIVATE: rden NUMERIC "0"
 // Retrieval info: LIBRARY: altera_mf altera_mf.altera_mf_components.all
 // Retrieval info: CONSTANT: ADDRESS_ACLR_A STRING "NONE"
@@ -142,14 +142,14 @@ endmodule
 // Retrieval info: CONSTANT: OUTDATA_ACLR_A STRING "NONE"
 // Retrieval info: CONSTANT: OUTDATA_REG_A STRING "UNREGISTERED"
 // Retrieval info: CONSTANT: WIDTHAD_A NUMERIC "19"
-// Retrieval info: CONSTANT: WIDTH_A NUMERIC "8"
+// Retrieval info: CONSTANT: WIDTH_A NUMERIC "2"
 // Retrieval info: CONSTANT: WIDTH_BYTEENA_A NUMERIC "1"
 // Retrieval info: USED_PORT: address 0 0 19 0 INPUT NODEFVAL "address[18..0]"
 // Retrieval info: USED_PORT: clock 0 0 0 0 INPUT VCC "clock"
-// Retrieval info: USED_PORT: q 0 0 8 0 OUTPUT NODEFVAL "q[7..0]"
+// Retrieval info: USED_PORT: q 0 0 2 0 OUTPUT NODEFVAL "q[1..0]"
 // Retrieval info: CONNECT: @address_a 0 0 19 0 address 0 0 19 0
 // Retrieval info: CONNECT: @clock0 0 0 0 0 clock 0 0 0 0
-// Retrieval info: CONNECT: q 0 0 8 0 @q_a 0 0 8 0
+// Retrieval info: CONNECT: q 0 0 2 0 @q_a 0 0 2 0
 // Retrieval info: GEN_FILE: TYPE_NORMAL title_rom.v TRUE
 // Retrieval info: GEN_FILE: TYPE_NORMAL title_rom.inc FALSE
 // Retrieval info: GEN_FILE: TYPE_NORMAL title_rom.cmp FALSE
